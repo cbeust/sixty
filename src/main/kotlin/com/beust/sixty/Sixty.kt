918715f68e282d34a526e9372dd533650ed789e5
Cedric Beust <cedric@beust.com>
2020-08-24 18:36:14 -0700

    Working on BNE

--- src/main/kotlin/com/beust/sixty/Sixty.kt
+++ src/main/kotlin/com/beust/sixty/Sixty.kt
@@ -88,19 +88,56 @@ class StackPointer {
     }
 }
 
+class StatusFlags {
+    private val bits = BitSet(8)
+
+    private fun bit(n: Int) = if (bits.get(n)) 1 else 0
+    private fun bit(n: Int, value: Int) = bits.set(n, value != 0)
+
+    var N: Int // Negative
+        get() = bit(7)
+        set(v) = bit(7, v)
+
+    var V: Int // Overflow
+        get() = bit(6)
+        set(v) = bit(6, v)
+
+    var D: Int // Decimal
+        get() = bit(3)
+        set(v) = bit(3, v)
+
+    var I: Int // Interrupt disable
+        get() = bit(2)
+        set(v) = bit(2, v)
+
+    var Z: Int // Zero
+        get() = bit(1)
+        set(v) = bit(1, v)
+
+    var C: Int // Carry
+        get() = bit(0)
+        set(v) = bit(0, v)
+
+    override fun toString() = "{N=$N V=$V D=$D I=$I Z=$Z C=$C}"
+}
+
 data class Cpu(var A: Byte = 0, var X: Byte = 0, var Y: Byte = 0, var PC: Int = 0,
-        val SP: StackPointer = StackPointer()) : ICpu {
+        val SP: StackPointer = StackPointer(), val P: StatusFlags = StatusFlags()) : ICpu {
     override fun nextInstruction(computer: Computer): Instruction {
         val op = computer.memory.byte(PC).toInt() and 0xff
         val result = when(op) {
             0x00 -> Brk(computer)
             0x20 -> Jsr(computer)
+            0x44 -> CpyImm(computer)
             0xff -> LdyImm(computer)
             0x60 -> Rts(computer)
             0x85 -> StaZp(computer)
             0x91 -> StaIndY(computer)
             0xa9 -> LdaImm(computer)
+            0xc8 -> Iny(computer)
+            0xd0 -> Bne(computer)
             0xe8 -> Inx(computer)
+            0xea -> Nop(computer)
             else -> TODO("NOT IMPLEMENTED: ${op.toHex()}")
         }
 
@@ -108,7 +145,7 @@ data class Cpu(var A: Byte = 0, var X: Byte = 0, var Y: Byte = 0, var PC: Int = 
     }
 
     override fun toString(): String {
-        return "{Cpu A=${A.toHex()} X=${X.toHex()} Y=${Y.toHex()} PC=${PC.toHex()} SP=$SP}"
+        return "{Cpu A=${A.toHex()} X=${X.toHex()} Y=${Y.toHex()} PC=${PC.toHex()} P=$P SP=$SP}"
     }
 }
 
@@ -118,7 +155,8 @@ abstract class InstructionBase(val computer: Computer): Instruction {
     val pc  by lazy { cpu.PC}
 //    val pc1 by lazy { cpu.PC + 1}
 //    val pc2 by lazy { cpu.PC + 2}
-    val b1 by lazy { memory.byte(cpu.PC + 1) }
+    val b1 by lazy { b1Signed.unsigned() }
+    val b1Signed by lazy { memory.byte(cpu.PC + 1) }
 //    val b2 by lazy { memory.byte(cpu.PC + 2) }
     val word by lazy { memory.byte(cpu.PC + 2).toInt().shl(8).or(memory.byte(cpu.PC + 1).toInt()) }
 }
@@ -143,6 +181,30 @@ class Jsr(c: Computer): InstructionBase(c) {
     override fun toString(): String = "JSR $${word.toHex()}"
 }
 
+fun Byte.unsigned() = java.lang.Byte.toUnsignedInt(this)
+
+/** 0x44, CPY $#12 */
+class CpyImm(c: Computer): InstructionBase(c) {
+    override val size = 2
+    override val timing = 2
+    override fun run() {
+        if (cpu.Y.toInt() != b1) {
+            cpu.P.Z = 0
+        } else {
+            cpu.P.Z = 1
+        }
+        if (cpu.Y.toInt() < b1) {
+            cpu.P.C = 0
+        } else {
+            cpu.P.C = 1
+        }
+        val sub = cpu.Y.toInt() - b1
+        cpu.P.N = sub.and(0x80).shr(7)
+    }
+
+    override fun toString(): String = "JSR $${word.toHex()}"
+}
+
 /** 0x44, LDY #$12 */
 class LdyImm(c: Computer): InstructionBase(c) {
     override val size = 2
@@ -151,7 +213,7 @@ class LdyImm(c: Computer): InstructionBase(c) {
     override fun toString(): String = "LDY #$" + memory.byte(cpu.PC + 1).toHex()
 }
 
-/** 0x60 */
+/** 0x60, RTS */
 class Rts(c: Computer): InstructionBase(c) {
     override val size = 1
     override val timing = 6
@@ -186,12 +248,39 @@ class LdaImm(c: Computer): InstructionBase(c) {
     override fun toString(): String = "LDA #$" + computer.memory.byte(computer.cpu.PC + 1).toHex()
 }
 
+/** 0xc8, INY */
+class Iny(c: Computer): InstructionBase(c) {
+    override val size = 1
+    override val timing = 2
+    override fun run() { cpu.Y++ }
+    override fun toString(): String = "INY"
+}
+
+/** 0xd0, BNE */
+class Bne(c: Computer): InstructionBase(c) {
+    override val size = 2
+    /** TODO(Varied timing if the branch is taken/not taken and if it crosses a page) */
+    override val timing = 2
+    override fun run() {
+        if (cpu.P.Z == 0) cpu.PC += b1Signed
+    }
+    override fun toString(): String = "BNE ${(cpu.PC - b1).toHex()}"
+}
+
 /** 0xe8, INX */
 class Inx(c: Computer): InstructionBase(c) {
     override val size = 1
     override val timing = 2
     override fun run() { cpu.X++ }
     override fun toString(): String = "INX"
+}
+
+/** 0xea, NOP */
+class Nop(c: Computer): InstructionBase(c) {
+    override val size = 1
+    override val timing = 2
+    override fun run() { }
+    override fun toString(): String = "NOP"
 }
 
 fun main() {
--- src/test/kotlin/com/beust/sixty/SixtyTest.kt
+++ src/test/kotlin/com/beust/sixty/SixtyTest.kt
@@ -7,12 +7,14 @@ import org.testng.annotations.Test
 class SixtyTest {
     fun computer(vararg bytes: Int) = Computer(memory = Memory(*bytes))
 
-    fun inx() {
-        // inx inx
-        computer(0xe8, 0xe8, 0).let { computer ->
+    fun inxy() {
+        // inx * 2, iny * 2
+        computer(0xe8, 0xe8, 0xc8, 0xc8, 0).let { computer ->
             assertThat(computer.cpu.X).isEqualTo(0)
+            assertThat(computer.cpu.Y).isEqualTo(0)
             computer.run()
             assertThat(computer.cpu.X).isEqualTo(2)
+            assertThat(computer.cpu.Y).isEqualTo(2)
         }
     }
 
@@ -75,9 +77,38 @@ class SixtyTest {
             computer.cpu.nextInstruction(computer).runDebug()
             assertThat(computer.memory.byte(10)).isEqualTo(0x42)
         }
-
     }
 
+    private fun cmpFlags(compared: Int, y: Byte, z: Int, c: Int, n: Int) {
+        computer(0x44, compared).let { computer ->
+            computer.cpu.Y = y
+            computer.cpu.P.let {
+                assertThat(it.Z).isEqualTo(0)
+                assertThat(it.C).isEqualTo(0)
+                assertThat(it.N).isEqualTo(0)
+            }
+            CpyImm(computer).runDebug()
+            computer.cpu.P.let {
+                assertThat(it.Z).isEqualTo(z)
+                assertThat(it.C).isEqualTo(c)
+                assertThat(it.N).isEqualTo(n)
+            }
+        }
+    }
+
+    fun cpyImm() {
+        // CPY #$80 with #$7F
+        cmpFlags(0x80, 0x7f, 0, 0, 1)
+
+        // CPY #$FF with 1
+        cmpFlags(0xff, 1, 0, 0, 0)
+    }
+
+    fun bne() {
+        computer(0xea, 0xd0, -3).let { computer ->
+            computer.run()
+        }
+    }
 /*
 $0600    a9 00     LDA #$00
 $0602    85 10     STA $10
