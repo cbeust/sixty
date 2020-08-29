1c0dc2a12b736794ee9c08f944502e76127a8286
Cedric Beust <cedric@beust.com>
2020-08-28 18:57:15 -0700

    Tests

--- src/test/kotlin/com/beust/sixty/HiResScreenTest.kt
+++ src/test/kotlin/com/beust/sixty/HiResScreenTest.kt
@@ -12,16 +12,16 @@ class HiResScreenTest {
      */
     @DataProvider
     fun bp() = arrayOf(
-//            arrayOf(0xf7, 0xee, 1, 1, 3, 1, 3, 2, 3, 1, 3)  // 1111_0111 1110_1110
-//            arrayOf(0xdd, 0xbb, 1, 1, 1, 3, 1, 3, 1, 3, 1)
-//            arrayOf(0x6e, 0x00, 1, 0, 2, 3, 2, 1, 0, 0, 0) // 0110_1110 0000_0000
-            arrayOf(0xe0, 0x00, 1, 0, 0, 0, 2, 1, 0, 0, 0)  // 1110_0000 0000_0000
+            arrayOf(0xf7, 0xee, 1, 1, 3, 1, 3, 1, 3, 1, 3)  // 1111_0111 1110_1110
+            , arrayOf(0xdd, 0xbb, 1, 1, 1, 3, 1, 3, 1, 3, 1)
+            , arrayOf(0x6e, 0x00, 0, 0, 2, 3, 2, 1, 0, 0, 0) // 0110_1110 0000_0000
+            , arrayOf(0xe0, 0x00, 1, 0, 0, 0, 2, 1, 0, 0, 0)  // 1110_0000 0000_0000
     )
 
     @Test(dataProvider = "bp")
     fun bitPatterns(byte0: Int, byte1: Int, p0: Int, p1: Int, a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) {
         with(BitPattern(byte0, byte1)) {
-            println(colorStrings())
+            println(colorStrings(0))
             assertThat(this.p0).isEqualTo(p0)
             assertThat(this.p1).isEqualTo(p1)
             assertThat(aa).isEqualTo(a)
