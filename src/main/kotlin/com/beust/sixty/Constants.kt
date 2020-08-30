64da725b091e21a34c35c59fb78e314dc613e866
Cedric Beust <cedric@beust.com>
2020-08-30 10:59:57 -0700

    Constants

--- src/main/kotlin/com/beust/sixty/Constants.kt
+++ src/main/kotlin/com/beust/sixty/Constants.kt
@@ -1,5 +1,23 @@
 package com.beust.sixty
 
+const val ADC_IMM = 0x69
+const val ADC_ZP = 0x65
+const val ADC_ZP_X = 0x75
+const val ADC_ABS = 0x6d
+const val ADC_ABS_X = 0x7d
+const val ADC_ABS_Y = 0x79
+const val ADC_IND_X = 0x61
+const val ADC_IND_Y = 0x71
+
+const val AND_IMM = 0x29
+const val AND_ZP = 0x25
+const val AND_ZP_X = 0x35
+const val AND_ABS = 0x24
+const val AND_ABS_X = 0x3d
+const val AND_ABS_Y = 0x39
+const val AND_IND_X = 0x21
+const val AND_IND_Y = 0x31
+
 const val LDY_IMM = 0xa0
 const val LDY_ZP = 0xa4
 const val LDY_ZP_X = 0xb4
