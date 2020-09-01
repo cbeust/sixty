443f8e1cb2a4499ef002c4717d7cd2ebce3aa1d0
Cedric Beust <cedric@beust.com>
2020-09-01 09:58:45 -0700

    ROL_ABS_X, 49678

--- src/main/kotlin/com/beust/sixty/Rol.kt
+++ src/main/kotlin/com/beust/sixty/Rol.kt
@@ -16,16 +16,22 @@ abstract class RolBase(c: Computer, override val opCode: Int, override val size:
     override fun toString(): String = "ROL${name}"
 }
 
+/** 0x2A, ROL */
+class Rol(c: Computer): RolBase(c, ROL, 1, 2) {
+    override var value by ValRegisterA()
+    override val name = nameA()
+}
+
 /** 0x26, ROL $12 */
 class RolZp(c: Computer): RolBase(c, ROL_ZP, 2, 5) {
     override var value by ValZp()
     override val name = nameZp()
 }
 
-/** 0x2A, ROL */
-class Rol(c: Computer): RolBase(c, ROL, 1, 2) {
-    override var value by ValRegisterA()
-    override val name = nameA()
+/** 0x36, ROL $12,X */
+class RolZpX(c: Computer): RolBase(c, ROL_ZP_X, 2, 6) {
+    override var value by ValZp()
+    override val name = nameZpX()
 }
 
 /** 0x2e, ROL $1234 */
@@ -34,8 +40,8 @@ class RolAbsolute(c: Computer): RolBase(c, ROL_ABS, 3, 6) {
     override val name = nameAbs()
 }
 
-/** 0x36, ROL $12,X */
-class RolZpX(c: Computer): RolBase(c, ROL_ZP_X, 2, 6) {
-    override var value by ValZp()
-    override val name = nameZpX()
+/** 0x3e, ROL $1234,X */
+class RolAbsoluteX(c: Computer): RolBase(c, ROL_ABS_X, 3, 7) {
+    override var value by ValAbsoluteX()
+    override val name = nameAbsX()
 }
