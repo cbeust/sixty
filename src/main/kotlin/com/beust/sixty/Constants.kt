d96e3c84cbd06c7b76080fcb1695e4e484e973a4
Cedric Beust <cedric@beust.com>
2020-08-31 23:00:14 -0700

    Constants

--- src/main/kotlin/com/beust/sixty/Constants.kt
+++ src/main/kotlin/com/beust/sixty/Constants.kt
@@ -10,13 +10,15 @@ const val ADC_IND_X = 0x61
 const val ADC_IND_Y = 0x71
 
 const val AND_IMM = 0x29
-const val AND_ZP = 0x25
+const val AND_ZP = 0x25c
 const val AND_ZP_X = 0x35
 const val AND_ABS = 0x24
 const val AND_ABS_X = 0x3d
 const val AND_ABS_Y = 0x39
 const val AND_IND_X = 0x21
 const val AND_IND_Y = 0x31
+
+const val ASL = 0x0a
 
 const val BIT_ZP = 0x24
 const val BIT_ABS = 0x2c
@@ -51,6 +53,8 @@ const val CMP_ABS_Y = 0xd9
 const val CMP_IND_X = 0xc1
 const val CMP_IND_Y = 0xd1
 
+const val CPY_IMM = 0xc0
+
 const val EOR_IMM = 0x49
 const val EOR_ZP = 0x45
 const val EOR_ZP_X = 0x55
@@ -60,6 +64,10 @@ const val EOR_ABS_Y = 0x59
 const val EOR_IND_X = 0x41
 const val EOR_IND_Y = 0x51
 
+const val INC_ZP = 0xe6
+
+const val JMP = 0x4c
+const val JMP_IND = 0x6c
 const val JSR = 0x20
 
 const val LDA_IMM = 0xa9
@@ -72,7 +80,6 @@ const val LDA_ABS_Y = 0xb9
 const val LDA_IND_X = 0xa1
 const val LDA_IND_Y = 0xb1
 
-
 const val LDY_IMM = 0xa0
 const val LDY_ZP = 0xa4
 const val LDY_ZP_X = 0xb4
@@ -82,7 +89,6 @@ const val LDY_ABS_X = 0xbc
 const val STY_ZP = 0x84
 const val STY_ZP_X = 0x94
 const val STY_ABS = 0x8c
-
 
 const val LSR_A = 0x4a
 const val LSR_ZP = 0x46
@@ -98,6 +104,8 @@ const val ORA_ABS_X = 0x1d
 const val ORA_ABS_Y = 0x19
 const val ORA_IND_X = 0x01
 const val ORA_IND_Y = 0x11
+
+const val NOP = 0xea
 
 const val ROL_A = 0x2a
 const val ROL_ZP = 0x26
