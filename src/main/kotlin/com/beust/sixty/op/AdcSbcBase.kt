1eecc77122505c02cbdc4ea55bde60e0101ae48a
Cedric Beust <cedric@beust.com>
2020-09-02 16:23:10 -0700

    Bug in ADC

--- src/main/kotlin/com/beust/sixty/op/AdcSbcBase.kt
+++ src/main/kotlin/com/beust/sixty/op/AdcSbcBase.kt
@@ -10,7 +10,7 @@ abstract class AdcSbcBase(c: Computer, override val opCode: Int, override val si
     fun add(v: Int) {
         var result: Int = cpu.A + v + cpu.P.C.int()
         val carry6: Int = cpu.A.and(0x7f) + v.and(0x7f) + cpu.P.C.int()
-        cpu.P.C = result.and(0x100) == 1
+        cpu.P.C = result.and(0x100) != 0
         cpu.P.V = cpu.P.C.xor((carry6.and(0x80) != 0))
         result = result and 0xff
         cpu.P.setNZFlags(result)
