4fd7c3c47ac66348416cb1c4e98f3afdc8102ff2
Cedric Beust <cedric@beust.com>
2020-09-15 11:50:35 -0700

    Better MemoryTest

--- src/test/kotlin/com/beust/sixty/MemoryTest.kt
+++ src/test/kotlin/com/beust/sixty/MemoryTest.kt
@@ -1,5 +1,7 @@
 package com.beust.sixty
 
+import org.assertj.core.api.Assertions.assertThat
+import org.testng.Assert
 import org.testng.annotations.Test
 import java.io.File
 
@@ -13,18 +15,21 @@ class MemoryTest {
             val ins2 = File("asm/ram").inputStream()
             load(ins2, start)
         }
+        var success = true
         val c = Computer(cpu = Cpu(memory = memory),
                 memoryListener = DebugMemoryListener()).apply {
             cpu.PC = start
             pcListener = object: PcListener {
                 override fun onPcChanged(c: Computer) {
                     if (memory[c.cpu.PC] == 0) {
-                        println("BRK ENCOUNTERED, TEST FAILED")
+                        success = false
                         stop()
                     }
                 }
             }
         }
         c.run()
+
+        assertThat(success).isTrue()
     }
 }
