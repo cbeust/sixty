6b06dff509fb70123a2e7ed2b6e815e90f0686f8
Cedric Beust <cedric@beust.com>
2020-09-15 11:05:42 -0700

    MemoryTest

--- src/test/kotlin/com/beust/sixty/MemoryTest.kt
+++ src/test/kotlin/com/beust/sixty/MemoryTest.kt
@@ -1,2 +1,30 @@
 package com.beust.sixty
 
+import org.testng.annotations.Test
+import java.io.File
+
+@Test
+class MemoryTest {
+
+    fun languageCard() {
+        val start = 0x300
+        val memory = Memory().apply {
+            val ins = this::class.java.classLoader.getResource("6502_functional_test.bin")?.openStream()
+            val ins2 = File("asm/ram").inputStream()
+            load(ins2, start)
+        }
+        val c = Computer(cpu = Cpu(memory = memory),
+                memoryListener = DebugMemoryListener()).apply {
+            cpu.PC = start
+            pcListener = object: PcListener {
+                override fun onPcChanged(c: Computer) {
+                    if (memory[c.cpu.PC] == 0) {
+                        println("BRK ENCOUNTERED, TEST FAILED")
+                        stop()
+                    }
+                }
+            }
+        }
+        c.run()
+    }
+}
