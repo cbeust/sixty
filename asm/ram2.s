f6a623ec653e12507ed0ac12a0cb0c605b000994
Cedric Beust <cedric@beust.com>
2020-09-16 17:25:41 -0700

    MemoryTest

--- asm/ram2.s
+++ asm/ram2.s
@@ -1,17 +1,7 @@
 D1 := $d17b
 FE := $fe1f
 checkdata := $3f
-
-    jsr @reset
-	;; Format:
-	;; Sequence of test instructions, finishing with `jsr .test`.
-	;; - quint: expected current $d17b and fe1f, then d17b in bank1, d17b in bank 2, and fe1f
-	;; (All sequences start with lda $C080, just to reset things to a known state.)
-	;; 0-byte to terminate tests.
-	lda $C088				; Read $C088 (RAM read, write protected)
-	jsr @test				;
-	.BYTE $11, $33, $11, $22, $33
-	rts
+TEST_COUNT := $3d
 
 .macro verify address
     ldx address
@@ -21,22 +11,57 @@ checkdata := $3f
     bne @fail
 .endmacro
 
+    jsr @reset
+    lda #0
+    sta TEST_COUNT
+
+	;; Format:
+	;; Sequence of test instructions, finishing with `jsr .test`.
+	;; - quint: expected current $d17b and fe1f, then d17b in bank1, d17b in bank 2, and fe1f
+	;; (All sequences start with lda $C080, just to reset things to a known state.)
+	;; 0-byte to terminate tests.
+
+	lda $C088				; Read $C088 (read bank 1, no write)
+	jsr @test				;
+	.BYTE $11, $33, $11, $22, $33
+
+	lda $C080				; Read $C080 (read bank 2, no write)
+	jsr @test				;
+	.BYTE $22, $33, $11, $22, $33		;
+
+	rts
+
 @test:
 	;; pull address off of stack: it points just below check data for this test.
 	pla
 	sta checkdata
+	tax
 	pla
 	sta checkdata+1
+	pha
+	txa
+	clc
+	adc #5
+	pha
+	ldy #0
 
-    verify D1 ; Test $D17B
-    verify FE ; Test $FE1F
-    lda $c088 ; Test $D17B in bank 1
     verify D1
-    lda $c080 ; Test $D17B in bank 2
+    verify FE
+    lda $c088 ; $D17B in bank 1
     verify D1
-    verify FE ; Test $FE1F
-
+    lda $c080 ; $D17B in bank 2
+    verify D1
+    verify FE
+    inc TEST_COUNT
     rts
+
+@success:
+    clc
+    rts
+
+@fail:
+    brk
+
 @reset:
 	;; Initialize to known state:
 	;; - $11 in $D17B bank 1 (ROM: $53)
@@ -55,6 +80,3 @@ checkdata := $3f
 	lda $C080
 	lda D1
 	rts
-
-@fail:
-    brk
--- src/test/kotlin/com/beust/sixty/MemoryTest.kt
+++ src/test/kotlin/com/beust/sixty/MemoryTest.kt
@@ -13,7 +13,7 @@ class MemoryTest {
         val start = 0x300
         val memory = Memory().apply {
             loadResource("Apple2e.rom", 0xc000)
-            val ins2 = File("asm/ram").inputStream()
+            val ins2 = File("asm/ram2").inputStream()
             load(ins2, start)
         }
         var success = true
@@ -26,7 +26,7 @@ class MemoryTest {
                 override fun onPcChanged(c: Computer) {
                     if (memory[c.cpu.PC] == 0) {
                         success = false
-                        message = "Failed at PC " + c.cpu.PC.hh()
+                        message = "Failed after " + memory[0x3d] + " tests, at PC " + c.cpu.PC.hh()
                         stop()
                     }
                 }
@@ -34,6 +34,11 @@ class MemoryTest {
         }
         c.run()
 
-        assertThat(success).withFailMessage(message ?: "").isTrue()
+        assertThat(success)
+                .withFailMessage(message ?: "")
+                .isTrue()
+//        assertThat(c.cpu.P.C)
+//                .withFailMessage("Failed after " + memory[0x3d] + " tests")
+//                .isFalse()
     }
 }
