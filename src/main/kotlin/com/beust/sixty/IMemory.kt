72337776b93d96d2e3c61a426e432bda52224d37
Cedric Beust <cedric@beust.com>
2020-09-22 10:16:05 -0700

    New files

---
+++ src/main/kotlin/com/beust/sixty/IMemory.kt
@@ -0,0 +1,39 @@
+package com.beust.sixty
+
+interface IMemory {
+    operator fun get(address: Int): Int
+    operator fun set(address: Int, value: Int)
+    fun forceValue(address: Int, value: Int)
+    fun load(allBytes: ByteArray, address: Int = 0, offset: Int = 0, size: Int = 0)
+    val listeners: ArrayList<MemoryListener>
+    val lastMemDebug: ArrayList<String>
+
+    fun init(address: Int, vararg bytes: Int) {
+        var ii = address
+        bytes.forEach { set(ii++, it) }
+    }
+
+    fun word(address: Int): Int = this[address].or(this[address + 1].shl(8))
+
+    fun loadResource(name: String, address: Int = 0, fileOffset: Int = 0, size: Int = 0) {
+        val bytes = this::class.java.classLoader.getResource(name).openStream().readAllBytes()
+        load(bytes, address, fileOffset, if (size == 0) bytes.size else size)
+    }
+
+    fun dump(address: Int, length: Int = 80) {
+        val lineLength = 8
+        repeat(length / lineLength) { line ->
+            val sb = StringBuffer(String.format("%04x", address + (line * lineLength)) + ": ")
+            repeat(8) { byte ->
+                sb.append(String.format("%02x ", this[address + line * lineLength + byte]))
+            }
+            sb.append(" ")
+            repeat(8) { byte ->
+                val c = (this[address + line * lineLength + byte] and 0x7f).toChar()
+                sb.append(String.format("%c", c))
+            }
+            println(sb.toString())
+        }
+        println("===")
+    }
+}
---
+++ src/main/kotlin/com/beust/sixty/SimpleMemory.kt
@@ -0,0 +1,35 @@
+package com.beust.sixty
+
+import java.io.InputStream
+
+class SimpleMemory(size: Int): IMemory {
+    override val listeners = arrayListOf<MemoryListener>()
+    override val lastMemDebug = arrayListOf<String>()
+    private val bytes = IntArray(size)
+
+    override fun get(address: Int): Int {
+        val result = bytes[address]
+        listeners.forEach { it.onRead(address, result) }
+        return result
+    }
+
+    override fun set(address: Int, value: Int) {
+        listeners.forEach { it.onWrite(address, value) }
+        bytes[address] = value
+    }
+
+    override fun forceValue(address: Int, value: Int) {
+        this[address] = value
+    }
+
+    override fun load(bytes: ByteArray, address: Int, offset: Int, size: Int) {
+        repeat(if (size == 0) bytes.size else size) {
+            this.bytes[it + address] = bytes[it + offset].toInt().and(0xff)
+        }
+    }
+
+    fun load(ins: InputStream, address: Int, fileOffset: Int, size: Int) {
+        val allBytes = ins.readBytes()
+        load(allBytes, address, fileOffset, size)
+    }
+}
