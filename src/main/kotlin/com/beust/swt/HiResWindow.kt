971ad256f7b5101e8103f27043620e5a2f70ac15
Cedric Beust <cedric@beust.com>
2020-09-27 11:35:23 -0700

    HiResWindow

--- src/main/kotlin/com/beust/swt/HiResWindow.kt
+++ src/main/kotlin/com/beust/swt/HiResWindow.kt
@@ -1,2 +1,21 @@
 package com.beust.swt
 
+import org.eclipse.swt.SWT
+import org.eclipse.swt.graphics.Rectangle
+import org.eclipse.swt.layout.GridData
+import org.eclipse.swt.widgets.Composite
+
+
+class HiResWindow(parent: Composite, style: Int = SWT.NONE): Composite(parent, style) {
+    init {
+        background = lightBlue(display)
+//        layoutData = GridData(GridData.FILL, GridData.FILL).apply {
+//            widthHint = 280
+//            heightHint = 102
+//        }
+        addPaintListener { e ->
+            val clientArea: Rectangle = shell.clientArea
+            e.gc.drawLine(0, 0, clientArea.width, clientArea.height)
+        }
+    }
+}
