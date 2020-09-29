0d3b8792ce23e1d867cdcedfa9bb7600e0313c24
Cedric Beust <cedric@beust.com>
2020-09-29 09:07:06 -0700

    Better UiState

--- src/main/kotlin/com/beust/app/UiState.kt
+++ src/main/kotlin/com/beust/app/UiState.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 typealias ObsListener<T> = (T, T) -> Unit
 
-class Obs<T>(val def: T) {
+class Obs<T>(val name: String, val def: T) {
     private val listeners = mutableListOf<ObsListener<T>>()
 
     fun addListener(l: (T, T) -> Unit) { listeners.add(l) }
@@ -13,7 +13,7 @@ class Obs<T>(val def: T) {
     var value: T
         get() = _value
         set(f) {
-            println("New value: $f")
+            println("$name=$f")
             _value = f
             listeners.forEach { it.invoke(_value, f) }
         }
@@ -24,14 +24,14 @@ class Obs<T>(val def: T) {
 enum class ByteAlgorithm { RAW, SHIFTED, DISK_CONTENT }
 
 object UiState {
-    var currentDiskFile: Obs<File?> = Obs(null)
-    var currentTrack = Obs(0)
+    var currentDiskFile: Obs<File?> = Obs("Current disk", null)
+    var currentTrack = Obs("Current track", 0)
 
-    var byteAlgorithn = Obs(ByteAlgorithm.SHIFTED)
-    val currentBytes: Obs<List<Int>> = Obs(emptyList())
+    var byteAlgorithn = Obs("Byte algorithm", ByteAlgorithm.SHIFTED)
+    val currentBytes: Obs<List<Int>> = Obs("Current bytes", emptyList())
 
-    var mainScreenText = Obs(true)
-    var mainScreenMixed = Obs(false)
-    var mainScreenHires = Obs(false)
-    var mainScreenPage2 = Obs(false)
+    var mainScreenText = Obs("text", true)
+    var mainScreenMixed = Obs("mixed", false)
+    var mainScreenHires = Obs("hires", false)
+    var mainScreenPage2 = Obs("page2", false)
 }
