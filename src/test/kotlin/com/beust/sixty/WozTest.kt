06cc1bdfd677874b036f51175a9573c456c93895
Cedric Beust <cedric@beust.com>
2020-10-19 22:18:20 -0700

    Boot test: Rescue Raiders

--- src/test/kotlin/com/beust/sixty/WozTest.kt
+++ src/test/kotlin/com/beust/sixty/WozTest.kt
@@ -30,12 +30,13 @@ class WozTest {
 
     data class DiskInfo(val disk: IDisk, val addresses: List<Pair<Int, Int>>, val seconds: Int = 5)
 
-    @DataProvider(parallel = true)
+    @DataProvider(parallel = false)
     private fun dp(): Array<Array<DiskInfo>> {
         fun create(name: String, addresses: List<Pair<Int, Int>>, seconds: Int = 5)
             = DiskInfo(IDisk.create(name, diskStream("boot/$name"))!!, addresses, seconds)
 
         val result = listOf(
+                create("Rescue Raiders - Disk 1, Side B.woz", listOf(0x8a8 to 0xe9), 10),
                 create("DOS 3.3.dsk", listOf(0xa000 to 0xad), 10),
                 create("DOS 3.3.woz", listOf(0xa000 to 0xad), 10),
                 create("Bouncing Kamungas.woz", listOf(0x1a3d to 0xe9)),
@@ -44,7 +45,8 @@ class WozTest {
                 create("Ultima4.dsk", listOf(0x6182 to 0x8e), 20),
                 create("Sherwood Forest.dsk", listOf(0x246d to 0x55), 10),
                 create("Blade of Blackpoole.dsk", listOf(0x6067 to 0x88), 10),
-                create("Commando - Disk 1, Side A.woz", listOf(0x2056 to 0x1e), 10)
+                create("Commando - Disk 1, Side A.woz", listOf(0x2056 to 0x1e), 10),
+                create("Planetfall - Disk 1, Side A.woz", listOf(0x154d to 0x10), 5)
             )
             .map { arrayOf(it) }
             .toTypedArray()
