c60ed46a62dac5137ea96fe270ed75b42d86fe70
Cedric Beust <cedric@beust.com>
2020-10-22 06:42:20 -0700

    Dead code

--- src/main/kotlin/com/beust/swt/DiskWindow.kt
+++ src/main/kotlin/com/beust/swt/DiskWindow.kt
@@ -11,7 +11,10 @@ import org.eclipse.swt.custom.StyleRange
 import org.eclipse.swt.custom.StyledText
 import org.eclipse.swt.events.SelectionEvent
 import org.eclipse.swt.events.SelectionListener
-import org.eclipse.swt.graphics.*
+import org.eclipse.swt.graphics.Color
+import org.eclipse.swt.graphics.Rectangle
+import org.eclipse.swt.graphics.TextLayout
+import org.eclipse.swt.graphics.TextStyle
 import org.eclipse.swt.layout.FillLayout
 import org.eclipse.swt.layout.GridData
 import org.eclipse.swt.layout.GridLayout
@@ -36,146 +39,145 @@ fun main(args: Array<String>) {
     }
 }
 
-fun test6(parent: Composite) {
-    val row1 = listOf(0xff to 2, 0xff to 2, 0xda to 0)
-            .map { TimedByte(it.first, it.second) }
-            .map { "<td>" + it.byte.h() +
-                  (if (it.timingBitCount > 0) "<sup>${it.timingBitCount}</sup>" else "") }
-            .joinToString("")
-    val row2 = listOf(0xff to 2, 0xda to 0, 0xff to 2)
-            .map { TimedByte(it.first, it.second)}
-            .map { "<td class=\"address\">" + it.byte.h() +
-                    (if (it.timingBitCount > 0) "<sup>${it.timingBitCount}</sup>" else "") }
-            .joinToString("")
-    val html = """
-            <head>
-                <style>
-                    table { font: 16px Courier; border-spacing: 0 }
-                    td { vertical-align:bottom }
-                    .superscript {color: red; vertical-align: 30%; font-size: 80%}
-                    .address { background-color: yellow }
-                    .data { background-color: lightblue}
-                    .offset { color: blue; font-weight: bold; padding-right: 10px; }
-                </style>
-            </head>
-            <table>
-            <tr>$row1</tr>
-            <tr>$row2</tr>
-            </table>
-            """
-    println(html)
-    Browser(parent, NONE).apply {
-        text = html
-
-    }
-}
-
-fun test5(parent: Shell) {
-    val display = parent.display
-//    val font1 = Fonts.font(display, "Tahoma", 14, SWT.BOLD)
-//    val font2 = Fonts.font(display, "MS Mincho", 20, SWT.ITALIC)
-//    val font3 = Fonts.font(display, "Arabic Transparent", 18, SWT.NORMAL)
-    val big = Fonts.font(parent.shell, "Courier New", 12, SWT.NORMAL)
-    val small = Fonts.font(parent.shell, "Courier New", 6, SWT.NORMAL)
-
-    val blue: Color = display.getSystemColor(SWT.COLOR_BLUE)
-    val green: Color = display.getSystemColor(SWT.COLOR_GREEN)
-    val yellow: Color = display.getSystemColor(SWT.COLOR_YELLOW)
-    val gray: Color = display.getSystemColor(SWT.COLOR_GRAY)
-
-    val layout = TextLayout(display).apply {
-        font = big
-    }
-    val style1 = TextStyle().apply {
-        rise = 4
-        font = small
-    }
-//    val style2 = TextStyle(font2, green, null)
-//    val style3 = TextStyle(font3, blue, gray)
-
-    layout.setText("FF2 FF2 D5\nFF FF2 AA")
-    layout.setStyle(style1, 2, 3)
-    layout.setStyle(style1, 6, 7)
-    layout.setStyle(style1, 16, 17)
-    parent.setBackground(display.getSystemColor(SWT.COLOR_WHITE))
-    parent.addListener(SWT.Paint, Listener { event -> layout.draw(event.gc, 10, 10) })
-
-}
-
-fun test4(shell: Shell) {
-    val tb = listOf(0xff to 2, 0xff to 2, 0xd5 to 0, 0xff to 10, 0xd5 to 0, 0xaa to 0).map {
-        TimedByte(it.first, it.second)
-    }
-//    createTextViewer(shell, tb.iterator())
-}
-
-fun test3(shell: Shell) {
-    val f = Fonts.font(shell, "Courier New", 14)
-    val f2 = Fonts.font(shell, "Courier New", 8)
-    val st = listOf(12, 16, 20, 24).map {
-        StyleRange().apply {
-            start = it
-            length = 2
-            background = red(shell.display)
-            rise = 5
-            font = f2
-        }
-    }.toTypedArray()
-    val text1 = "FF FF D5 FF"
-    val text2 = " 2   2   0   3 "
-    TextViewer(shell, SWT.NONE).apply {
-        with(textWidget) {
-            text = "$text1\n$text2"
-            font = f
-            styleRanges = st
-        }
-    }
-}
-
-
-fun test2(shell: Shell) {
-    val f = Fonts.font(shell, "Courier New", 14)
-    val f2 = Fonts.font(shell, "Courier New", 8)
-    Composite(shell, SWT.NONE).apply {
-        background = yellow(display)
-        layout = GridLayout(1, false).apply {
-            marginHeight = 0
-            verticalSpacing = 0
-            marginBottom = 30
-        }
-        label(this, "FF FF D5 FF").apply {
-            font = f
-        }
-        label(this, "2  2  0  3").apply {
-//            background = blue(display)
-            layoutData = GridData(SWT.CENTER, SWT.BEGINNING, true, true)
-            font = f2
-        }
-    }
-}
-
-
-fun test1(shell: Shell) {
-    val f = Fonts.font(shell, "Courier New", 14)
-    val f2 = Fonts.font(shell, "Courier New", 8)
-    val st = listOf(3).map {
-        StyleRange().apply {
-            start = it
-            length = 2
-            rise = 5
-            font = f2
-        }
-    }.toTypedArray()
-    Composite(shell, SWT.NONE).apply {
-        layout = FillLayout()
-        StyledText(this, SWT.BORDER).apply {
-            font = f
-            text = "FF\n 2"
-            styleRanges = st
-        }
-    }
-}
-
+//fun test6(parent: Composite) {
+//    val row1 = listOf(0xff to 2, 0xff to 2, 0xda to 0)
+//            .map { TimedByte(it.first, it.second) }
+//            .map { "<td>" + it.byte.h() +
+//                  (if (it.timingBitCount > 0) "<sup>${it.timingBitCount}</sup>" else "") }
+//            .joinToString("")
+//    val row2 = listOf(0xff to 2, 0xda to 0, 0xff to 2)
+//            .map { TimedByte(it.first, it.second)}
+//            .map { "<td class=\"address\">" + it.byte.h() +
+//                    (if (it.timingBitCount > 0) "<sup>${it.timingBitCount}</sup>" else "") }
+//            .joinToString("")
+//    val html = """
+//            <head>
+//                <style>
+//                    table { font: 16px Courier; border-spacing: 0 }
+//                    td { vertical-align:bottom }
+//                    .superscript {color: red; vertical-align: 30%; font-size: 80%}
+//                    .address { background-color: yellow }
+//                    .data { background-color: lightblue}
+//                    .offset { color: blue; font-weight: bold; padding-right: 10px; }
+//                </style>
+//            </head>
+//            <table>
+//            <tr>$row1</tr>
+//            <tr>$row2</tr>
+//            </table>
+//            """
+//    println(html)
+//    Browser(parent, NONE).apply {
+//        text = html
+//    }
+//}
+//
+//fun test5(parent: Shell) {
+//    val display = parent.display
+////    val font1 = Fonts.font(display, "Tahoma", 14, SWT.BOLD)
+////    val font2 = Fonts.font(display, "MS Mincho", 20, SWT.ITALIC)
+////    val font3 = Fonts.font(display, "Arabic Transparent", 18, SWT.NORMAL)
+//    val big = Fonts.font(parent.shell, "Courier New", 12, SWT.NORMAL)
+//    val small = Fonts.font(parent.shell, "Courier New", 6, SWT.NORMAL)
+//
+//    val blue: Color = display.getSystemColor(SWT.COLOR_BLUE)
+//    val green: Color = display.getSystemColor(SWT.COLOR_GREEN)
+//    val yellow: Color = display.getSystemColor(SWT.COLOR_YELLOW)
+//    val gray: Color = display.getSystemColor(SWT.COLOR_GRAY)
+//
+//    val layout = TextLayout(display).apply {
+//        font = big
+//    }
+//    val style1 = TextStyle().apply {
+//        rise = 4
+//        font = small
+//    }
+////    val style2 = TextStyle(font2, green, null)
+////    val style3 = TextStyle(font3, blue, gray)
+//
+//    layout.setText("FF2 FF2 D5\nFF FF2 AA")
+//    layout.setStyle(style1, 2, 3)
+//    layout.setStyle(style1, 6, 7)
+//    layout.setStyle(style1, 16, 17)
+//    parent.setBackground(display.getSystemColor(SWT.COLOR_WHITE))
+//    parent.addListener(SWT.Paint, Listener { event -> layout.draw(event.gc, 10, 10) })
+//
+//}
+//
+//fun test4(shell: Shell) {
+//    val tb = listOf(0xff to 2, 0xff to 2, 0xd5 to 0, 0xff to 10, 0xd5 to 0, 0xaa to 0).map {
+//        TimedByte(it.first, it.second)
+//    }
+////    createTextViewer(shell, tb.iterator())
+//}
+//
+//fun test3(shell: Shell) {
+//    val f = Fonts.font(shell, "Courier New", 14)
+//    val f2 = Fonts.font(shell, "Courier New", 8)
+//    val st = listOf(12, 16, 20, 24).map {
+//        StyleRange().apply {
+//            start = it
+//            length = 2
+//            background = red(shell.display)
+//            rise = 5
+//            font = f2
+//        }
+//    }.toTypedArray()
+//    val text1 = "FF FF D5 FF"
+//    val text2 = " 2   2   0   3 "
+//    TextViewer(shell, SWT.NONE).apply {
+//        with(textWidget) {
+//            text = "$text1\n$text2"
+//            font = f
+//            styleRanges = st
+//        }
+//    }
+//}
+//
+//
+//fun test2(shell: Shell) {
+//    val f = Fonts.font(shell, "Courier New", 14)
+//    val f2 = Fonts.font(shell, "Courier New", 8)
+//    Composite(shell, SWT.NONE).apply {
+//        background = yellow(display)
+//        layout = GridLayout(1, false).apply {
+//            marginHeight = 0
+//            verticalSpacing = 0
+//            marginBottom = 30
+//        }
+//        label(this, "FF FF D5 FF").apply {
+//            font = f
+//        }
+//        label(this, "2  2  0  3").apply {
+////            background = blue(display)
+//            layoutData = GridData(SWT.CENTER, SWT.BEGINNING, true, true)
+//            font = f2
+//        }
+//    }
+//}
+//
+//
+//fun test1(shell: Shell) {
+//    val f = Fonts.font(shell, "Courier New", 14)
+//    val f2 = Fonts.font(shell, "Courier New", 8)
+//    val st = listOf(3).map {
+//        StyleRange().apply {
+//            start = it
+//            length = 2
+//            rise = 5
+//            font = f2
+//        }
+//    }.toTypedArray()
+//    Composite(shell, SWT.NONE).apply {
+//        layout = FillLayout()
+//        StyledText(this, SWT.BORDER).apply {
+//            font = f
+//            text = "FF\n 2"
+//            styleRanges = st
+//        }
+//    }
+//}
+//
 
 class DiskWindow(parent: Composite): Composite(parent, NONE) {
     private lateinit var diskLabel: Button
